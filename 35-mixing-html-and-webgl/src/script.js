77532af3cb1fadb904a3a0ed72636ecf6d6952b6
Kisko Ortiz <fortizgamez@gmail.com>
2022-03-21 15:26:40 +0100

    35 - Mixing with HTML 2

--- 35-mixing-html-and-webgl/src/script.js
+++ 35-mixing-html-and-webgl/src/script.js
@@ -3,10 +3,12 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { gsap } from 'gsap'
+import { Raycaster } from 'three'
 
 /**
  * Loaders
  */
+let sceneReady = false;
 const loadingBarElement = document.querySelector('.loading-bar')
 const loadingManager = new THREE.LoadingManager(
     // Loaded
@@ -22,6 +24,11 @@ const loadingManager = new THREE.LoadingManager(
             loadingBarElement.classList.add('ended')
             loadingBarElement.style.transform = ''
         }, 500)
+
+        window.setTimeout(() =>
+        {
+            sceneReady = true;
+        }, 2000)
     },
 
     // Progress
@@ -132,12 +139,23 @@ gltfLoader.load(
 /**
  * Points of Interest
  */
+const raycaster = new Raycaster();
 const points = [
     {
         position: new THREE.Vector3(1.55, 0.3, -0.6),
         element: document.querySelector('.point-0')
+    },
+    {
+        position: new THREE.Vector3(0.5, 0.8, -1.6),
+        element: document.querySelector('.point-1')
+    },
+    {
+        position: new THREE.Vector3(1.6, -1.3, -0.7),
+        element: document.querySelector('.point-2')
     }
 ];
+
+
 
 /**
  * Lights
@@ -210,13 +228,31 @@ const tick = () =>
     controls.update()
 
     // Update Points
-    for(const point of points) {
-        const screenPosition = point.position.clone();
-        screenPosition.project(camera);
-
-        const translateX = screenPosition.x * sizes.width * 0.5;
-        const translateY = - screenPosition.y * sizes.height * 0.5;
-        point.element.style.transform = `translate(${translateX}px, ${translateY}px)`;
+    if (sceneReady) {
+        for(const point of points) {
+            const screenPosition = point.position.clone();
+            screenPosition.project(camera);
+    
+            raycaster.setFromCamera(screenPosition, camera);
+            const intersects = raycaster.intersectObjects(scene.children, true);
+    
+            if (intersects.length === 0) {
+                point.element.classList.add('visible');
+            } else {
+                const intersectionDistance = intersects[0].distance;
+                const pointDistance = point.position.distanceTo(camera.position);
+                if (intersectionDistance < pointDistance) {
+                    point.element.classList.remove('visible');    
+                } else {
+                    point.element.classList.add('visible');
+                }
+                
+            }
+    
+            const translateX = screenPosition.x * sizes.width * 0.5;
+            const translateY = - screenPosition.y * sizes.height * 0.5;
+            point.element.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        }
     }
 
     // Render
