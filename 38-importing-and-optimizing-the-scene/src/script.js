8fd0d3075cd6f04056f3db4e285da12245faaf9d
Kisko Ortiz <fortizgamez@gmail.com>
2022-03-24 18:03:49 +0100

    Final Class

--- 38-importing-and-optimizing-the-scene/src/script.js
+++ 38-importing-and-optimizing-the-scene/src/script.js
@@ -5,10 +5,16 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
+import fireFliesVertexShader from './shaders/fireflies/vertex.glsl';
+import fireFliesFragmentShader from './shaders/fireflies/fragment.glsl';
+import portalVertexShader from './shaders/portal/vertex.glsl';
+import portalFragmentShader from './shaders/portal/fragment.glsl';
+
 /**
  * Base
  */
 // Debug
+const debugObject = {}
 const gui = new dat.GUI({
     width: 400
 })
@@ -50,12 +56,33 @@ bakedTexture.encoding = THREE.sRGBEncoding;
 
 // scene.add(cube)
 
+debugObject.portalColorStart = '#ff0000';
+debugObject.portalColorEnd = '#0000ff';
+
+gui.addColor(debugObject, 'portalColorStart').onChange(() => {
+    portalMaterial.uniforms.uColorStart.value.set(debugObject.portalColorStart);
+});
+
+gui.addColor(debugObject, 'portalColorEnd').onChange(() => {
+    portalMaterial.uniforms.uColorEnd.value.set(debugObject.portalColorEnd);
+});
+
 /**
  * Material
  */
 const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture });
-const portalMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
 const poleLightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffe5 });
+const portalMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        uTime: { value: 0},
+        uColorStart: { value: new THREE.Color(debugObject.portalColorStart) },
+        uColorEnd: { value: new THREE.Color(debugObject.portalColorEnd) }
+    },
+    vertexShader: portalVertexShader,
+    fragmentShader: portalFragmentShader
+});
+
+
 
 /**
  * Model
@@ -78,6 +105,43 @@ gltfLoader.load(
 )
 
 /**
+ * Particles
+ */
+const fireFliesGeometry = new THREE.BufferGeometry();
+const fireFliesCount = 30;
+const positionArray = new Float32Array(fireFliesCount * 3);
+const scaleArray = new Float32Array(fireFliesCount);
+
+for(let i = 0; i < fireFliesCount; i++) {
+    positionArray[i * 3 + 0] = (Math.random() - 0.5) * 4;
+    positionArray[i * 3 + 1] = Math.random() * 1.5;
+    positionArray[i * 3 + 2] = (Math.random() - 0.5) * 4;
+    scaleArray[i] = Math.random();
+}
+
+fireFliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3));
+fireFliesGeometry.setAttribute('aScale', new THREE.BufferAttribute(scaleArray, 1));
+
+const fireFliesMaterial = new THREE.ShaderMaterial({
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+    uniforms: 
+    {
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+        uSize: { value: 100 },
+        uTime: { value: 0 }
+    },
+    vertexShader: fireFliesVertexShader,
+    fragmentShader: fireFliesFragmentShader
+});
+gui.add(fireFliesMaterial.uniforms.uSize, 'value').min(0).max(500).step(1).name('fireflies size');
+
+const fireFlies = new THREE.Points(fireFliesGeometry, fireFliesMaterial);
+scene.add(fireFlies);
+
+
+/**
  * Sizes
  */
 const sizes = {
@@ -98,6 +162,8 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    fireFliesMaterial.uniforms.uPixelRatio.value = Math.min(window.devicePixelRatio, 2);
 })
 
 /**
@@ -125,6 +191,11 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+debugObject.clearColor = '#211212';
+gui.addColor(debugObject, 'clearColor').onChange(() => {
+    renderer.setClearColor(debugObject.clearColor);
+});
+
 /**
  * Animate
  */
@@ -136,6 +207,9 @@ const tick = () =>
 
     // Update controls
     controls.update()
+
+    fireFliesMaterial.uniforms.uTime.value = elapsedTime;
+    portalMaterial.uniforms.uTime.value = elapsedTime;
 
     // Render
     renderer.render(scene, camera)
---
+++ 38-importing-and-optimizing-the-scene/src/shaders/fireflies/fragment.glsl
@@ -0,0 +1,6 @@
+void main()
+{
+  float distanceFromCenter = distance(gl_PointCoord, vec2(0.5));
+  float strength = 0.05 / distanceFromCenter - 0.1;
+  gl_FragColor = vec4(1.0, 1.0, 1.0, strength);
+}
